e56361b37128ba12162767244244eb8abcde243d
ELYOUSFI Mohamed <mohamed5elyousfi@gmail.com>
2022-04-11 14:58:45 +0200

    testSellDatePassed + testQualityNegativeValue + testAgedBrieIncreaseTheOlderItGets

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,7 +11,33 @@ class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
+
+    @Test
+    void testSellDatePassed() {
+        Item[] items = new Item[] { new Item("item1", 0, 30) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(28, app.items[0].quality);
+    }
+
+    @Test
+    void testQualityNegativeValue() {
+        Item[] items = new Item[] { new Item("item2", 5, -5) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(true, app.items[0].quality < 0);
+    }
+
+    @Test
+    void testAgedBrieIncreaseTheOlderItGets() {
+        Item[] items = new Item[] { new Item("Aged Brie", 5, 5) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(true, app.items[0].quality > 5);
+    }
+
+
 
 }
