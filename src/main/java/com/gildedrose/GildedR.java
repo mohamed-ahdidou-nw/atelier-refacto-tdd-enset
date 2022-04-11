468616cdb675c0fddf46766e52e56365a8da5713
bertrand <bertrandkafando07@gmail.com>
2022-04-11 16:15:54 +0000

    workshop

--- src/main/java/com/gildedrose/GildedR.java
+++ src/main/java/com/gildedrose/GildedR.java
@@ -1,2 +1,39 @@
-package com.gildedrose;public class GildedR {
+package com.gildedrose;
+
+public class GildedR {
+    Item[] items;
+
+    public GildedR(Item[] items) {
+        this.items = items;
+    }
+
+    public  void updateQuality(){
+        for( Item it:items){
+           switch (it.name){
+               case "Backstage passes to a TAFKAL80ETC concert" :  updateBackstageAndAgeBrie(it);
+               case "Aged Brie" : updateBackstageAndAgeBrie(it);
+               case "Sulfuras, Hand of Ragnaros" :;
+
+               default: ;
+           }
+        }
+    }
+    public void updateBackstageAndAgeBrie(Item it){
+        //Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+        if (it.quality < 50) {  //	Quality drops to 0 after the concert
+            if(it.sellIn==0){
+                it.quality=0;
+            }
+            else if (it.sellIn<=5){ //incr  by 3
+                it.quality= it.quality+3;
+            }
+            else if(it.sellIn<=10){ //incr by 2
+                it.quality= it.quality+2;
+            }else it.quality= it.quality+1; //incr by 1
+
+        }
+    }
+
+
 }
+
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,7 +11,65 @@ class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
+
+    @Test
+    void NormalQualite(){
+        Item[] items = new Item[] { new Item("", 0, 4) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(2, app.items[0].quality);
+
+    }
+    @Test
+    void testNegativeQuality() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 9) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(app.items[0].quality>=0,true);
+    }
+    @Test
+    void testQualityNeverMoreThanFifty() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 3, 49) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(app.items[0].quality>=50,false);
+    }
+
+    @Test
+    void sellinPastBackstage(){
+        //Backstage avec un sellin past Quality drops to 0 after the concert
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", -1, 49) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0,app.items[0].quality);
+    }
+
+    @Test
+    void sellinPastAgeBrieQuality(){
+        // Once the sell by date has passed, Quality degrades twice as fast
+        Item[] items = new Item[] { new Item("", -2, 40) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(app.items[0].quality==38,true);
+    }
+
+    //"Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+    @Test
+    void sulfurasTestLegendary(){
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", -2, 40) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(app.items[0].quality==40,true);
+    }
+
+
+
+
+
+
+
+
 
 }
