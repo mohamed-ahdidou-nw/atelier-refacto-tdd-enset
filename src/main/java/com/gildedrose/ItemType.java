c1209b1d4640faa9ac086d785452d40152125a76
Yasser JANAH <contact@yasser-janah.com>
2022-04-11 15:10:21 +0000

    create ItemType class that contains all standart types

---
+++ src/main/java/com/gildedrose/ItemType.java
@@ -0,0 +1,7 @@
+package com.gildedrose;
+
+public class ItemType {
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String BACKSTAGE_PASSES_TO_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+}
