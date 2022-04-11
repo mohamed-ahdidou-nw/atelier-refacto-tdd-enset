bd72b1a6c1fc809f3ba3158ba2100c9e04afb429
Yasser JANAH <contact@yasser-janah.com>
2022-04-11 15:24:35 +0000

    Refactor the code

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,6 @@
 package com.gildedrose;
 
+
 class GildedRose {
     Item[] items;
 
@@ -8,55 +9,95 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+        for (Item item : items) {
+            // check if the Quality is not less than 0 or higher than 50
+            // I've noticed that the old code checks for quality:
+            //  - is higher than 0 => 2 times
+            //  - is less than 50 => 4 times
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+            if (item.quality < 50 && item.quality > 0) {
+                this._updateQuality(item);
             }
         }
+    }
+
+    private void _updateQuality(Item item){
+
+        switch (item.name){
+            case ItemType.AGED_BRIE:
+                if(item.sellIn < 1) item.quality = Math.min(50 , item.quality + 2);
+                else item.quality = Math.min(50 , item.quality + 1);
+                item.sellIn = item.sellIn - 1;
+                break;
+            case ItemType.BACKSTAGE_PASSES_TO_CONCERT:
+                if (item.sellIn < 11) item.quality += 1;
+                else item.quality += 1;
+                item.sellIn = item.sellIn - 1;
+                break;
+            default:
+                System.out.println("HERE");
+                if(item.sellIn == 0) item.quality = Math.max(0,item.quality -2);
+                else item.quality = Math.max(0,item.quality -1);
+                item.sellIn = item.sellIn - 1;
+        }
+
+        /*
+        // check if the category is Backstage passes to a TAFKAL80ETC concert
+        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
+            if (item.sellIn < 11) item.quality += 1;
+            if (item.sellIn < 6) item.quality += 1;
+        }
+
+        if (!item.name.equals("Aged Brie")
+            && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            //if (item.quality > 0) { // check if the Quality higher than 0 (1 time)
+                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    item.quality = item.quality - 1;
+                }
+           // }
+        } else {
+           // if (item.quality < 50) { // check if quality is less than 50 (1 time)
+                item.quality = item.quality + 1;
+
+                /*
+                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.sellIn < 11) {
+                        //if (item.quality < 50) { // check if quality is less than 50 (2 time)
+                            item.quality = item.quality + 1;
+                       // }
+                    }
+
+                    if (item.sellIn < 6) {
+                        //if (item.quality < 50) { // check if quality is less than 50 (3 time)
+                            item.quality = item.quality + 1;
+                       // }
+                    }
+                }
+           // }
+        }
+
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.sellIn = item.sellIn - 1;
+        }
+
+        if (item.sellIn < 0) {
+            if (!item.name.equals("Aged Brie")) {
+                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    // if (item.quality > 0) { // check if the Quality is higher than 0 (2 time)
+                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            item.quality = item.quality - 1;
+                        }
+                    // }
+                } else {
+                    // item.quality = item.quality - item.quality;
+                    item.quality = 0;
+                }
+            } else {
+                // if (item.quality < 50) { // check if quality is higher than 50 (4 time)
+                    item.quality = item.quality + 1;
+                //}
+            }
+        }
+        */
     }
 }
