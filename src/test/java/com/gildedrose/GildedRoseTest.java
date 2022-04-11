6d55a27876cbd06e0de21b9a811ad79207dba17b
Yasser JANAH <contact@yasser-janah.com>
2022-04-11 15:31:57 +0000

    add tests

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,4 +14,46 @@ class GildedRoseTest {
         assertEquals("fixme", app.items[0].name);
     }
 
+    @Test
+    void testQualityIfNegative(){
+        Item[] items = new Item[]{
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 3)
+        };
+        int oldQuality = items[0].quality;
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        if(gildedRose.items[0].sellIn < 6) {
+            assertEquals(oldQuality + 2  ,gildedRose.items[0].quality);
+        }
+        if(gildedRose.items[0].sellIn < 11) {
+            assertEquals(oldQuality + 1  ,gildedRose.items[0].quality);
+        }
+
+    }
+
+    @Test
+    void OnceTheSellByDateHasPassedQualityDegradesTwiceAsFast(){
+        Item[] items = new Item[]{
+            new Item("Any Other Catogory", 0, 100)
+        };
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assertEquals(100, gildedRose.items[0].quality);
+    }
+
+    @Test
+    void AgedBrieActuallyIncreasesInQualityTheOlderItGets(){
+        Item[] items = new Item[]{
+            new Item("Aged Brie", 10, 10)
+        };
+        int expected_value = items[0].quality;
+        if (items[0].sellIn <= 0){
+            expected_value += 2;
+        } else expected_value += 1;
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assertEquals(expected_value, gildedRose.items[0].quality);
+    }
+
+
 }
