78fb7c5a3b0ee815e282fc94dde2ed180127affd
Yasser JANAH <contact@yasser-janah.com>
2022-04-12 12:16:57 +0000

    Remove a code accidentally merged

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,6 +3,7 @@ package com.gildedrose;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GildedRoseTest {
 
@@ -11,7 +12,46 @@ class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    void testSellDatePassedDecreaseBy2() {
+        Item[] items = new Item[] { new Item("item1", 0, 30) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(28, app.items[0].quality);
+    }
+
+    @Test
+    void testSulfurasNeverSoldOrDescreaseInQuality() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 5, 15) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(true, app.items[0].quality == 15 && app.items[0].sellIn == 5);
+    }
+
+    @Test
+    void testAgedBrieIncreaseTheOlderItGets() {
+        Item[] items = new Item[] { new Item("Aged Brie", 5, 5) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertTrue(app.items[0].quality > 5);
+    }
+
+    @Test
+    void testQualityNegativeValue() {
+        Item[] items = new Item[] { new Item("item2", 5, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertTrue(app.items[0].quality == 0);
+    }
+
+    @Test
+    void testQualityOfItemMoreThan50() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 11, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertTrue(app.items[0].quality == 50);
+    }
 }
