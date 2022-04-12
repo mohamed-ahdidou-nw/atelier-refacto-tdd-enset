b672fe04488c08a2683cbb9e2e31002afe63ed45
Fatima Zahra Hasbi <hasbi.fatimazahra04@gmail.com>
2022-04-12 12:46:22 +0000

    my commit

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,4 +14,38 @@ class GildedRoseTest {
         assertEquals("fixme", app.items[0].name);
     }
 
+    @Test
+    void givenQualitieInContructorIsTheSameinResultAfterInstantiating(){
+        Item[] items = new Item[] { new Item("foo", 0, 10) };
+        GildedRose app = new GildedRose(items);
+        assertEquals(10, app.items[0].quality);
+    }
+
+
+    @Test
+    void TheQualityOfAnItemIsNeverNegative(){
+        int quality=-10;
+        Item[] items = new Item[] { new Item("foo", 2, quality) };
+        GildedRose app = new GildedRose(items);
+        assertEquals(quality>0 , true);
+    }
+
+    @Test
+    void firstConditionTest(){
+        int quality=10;
+        Item[] items = new Item[] { new Item("item1", 0, quality) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(quality-1 , app.items[0].quality);
+    }
+
+    @Test
+    void AgedBrieIncreaseWithTime(){
+        int quality = 12;
+        Item[] items = new Item[]{new Item("Aged Brie", 4, quality)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(quality< items[0].quality, true);
+    }
+
 }
